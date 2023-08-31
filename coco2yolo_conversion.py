2a65665a5c14a111c78cf4cdb689a966f889225e
Shubhabrata Mukherjee <shubha07m@gmail.com>
2023-08-31 14:17:53 -0700

    Upgrade coco2yolo_conversion.py
    1. Added option for testing with small samples.
    2. Added multiprocessing  feature for Process-based parallelism.

--- coco2yolo_conversion.py
+++ coco2yolo_conversion.py
@@ -5,16 +5,22 @@ License: MIT License (https://opensource.org/licenses/MIT).
 Description:
 This script convert a coco json format dataset to YOLO format.
 The current one uses a FLIR V2 infrared dataset as an example.
-Make sure the source (unzipped folder) and this script is in
-the same path.
+
+Note:
+1. Make sure the source (unzipped folder) and this script is in
+the same path. 
+2. For testing with a small samples (default 10)
+select 1, else use 0 as argument.
+3. This script use multiprocessing for Process-based parallelism.
 
 Author: Shubh
-Date: August 28, 2023
+Date: August 31, 2023
 """
 import json
 import os
 import shutil
 import time
+from multiprocessing import Process
 
 
 t = time.time()
@@ -39,19 +45,19 @@ def folder_creation(base_path):
 def get_data_type(part, thermal):
     if thermal:
         return {
-            'train': 'images_rgb_train',
-            'val': 'images_rgb_val',
-            'test': 'video_rgb_test'
-        }[part]
-    else:
-        return {
             'train': 'images_thermal_train',
             'val': 'images_thermal_val',
             'test': 'video_thermal_test'
         }[part]
+    else:
+        return {
+            'train': 'images_rgb_train',
+            'val': 'images_rgb_val',
+            'test': 'video_rgb_test'
+        }[part]
 
 
-def save_images_folder(data_part, thermal):
+def save_images_folder(thermal, data_part):
     file_names = []
     database = get_data_type(data_part, thermal)
     count = 0
@@ -68,40 +74,43 @@ def save_images_folder(data_part, thermal):
         file_names.append(filename)
         count += 1
 
+        if count > count_max:
+            break
+
     return file_names
 
 
-def get_coco_jsonfile(data_type, thermal):
+def get_coco_jsonfile(thermal, data_type):
     database = get_data_type(data_type, thermal)
     with open(os.path.join(input_path, database, 'coco.json')) as f:
         cocodata = json.load(f)
     return cocodata
 
 
-def get_img_ann(image_id, data_type, thermal):
-    data = get_coco_jsonfile(data_type, thermal)
+def get_img_ann(image_id, thermal, data_type):
+    data = get_coco_jsonfile(thermal, data_type)
     img_ann = [ann for ann in data['annotations'] if ann['image_id'] == image_id]
     return img_ann
 
 
-def get_img(filename, data_type, thermal):
-    data = get_coco_jsonfile(data_type, thermal)
+def get_img(filename, thermal, data_type):
+    data = get_coco_jsonfile(thermal, data_type)
     img = next((img for img in data['images'] if img['file_name'][5:] == filename), None)
     return img
 
 
-def save_annotation_folder(data_type, thermal):
-    fnames = save_images_folder(data_type, thermal)
+def save_annotation_folder(thermal, data_type):
+    fnames = save_images_folder(thermal, data_type)
     count = 0
     for filename in fnames:
-        img = get_img(filename, data_type,thermal)
+        img = get_img(filename, thermal, data_type)
         if img is None:
             print(f"Image not found: {filename} in {data_type} dataset.")
             continue
         img_id = img['id']
         img_w = img['width']
         img_h = img['height']
-        img_ann = get_img_ann(img_id, data_type, thermal)
+        img_ann = get_img_ann(img_id, thermal, data_type)
 
         if img_ann:
             folder = 'flirv2_thermal' if thermal else 'flirv2_rgb'
@@ -127,16 +136,29 @@ def save_annotation_folder(data_type, thermal):
 
             count += 1
 
+            if count > count_max:
+                break
+
 
 if __name__ == '__main__':
     current_path = os.getcwd()
     new_yolodata_path = folder_creation(current_path)
 
     # For testing only#
-    # save_annotation_folder('val', 0)
 
-    for data_type in ['train', 'val', 'test']:
-        for thermal in [0, 1]:
-            save_annotation_folder(data_type, thermal)
+    print('testing or actual - enter 1 for testing, else 0:\n')
+
+    testing = int(input())
+    if testing:
+        count_max = 9
+    else:
+        count_max = 50000
+
+
+    for thermal in [0, 1]:
+        for data_type in ['train', 'val', 'test']:
+            p = Process(target=save_annotation_folder, args=(thermal, data_type))
+            p.start()
+            p.join()
 
 print(time.time() - t)
